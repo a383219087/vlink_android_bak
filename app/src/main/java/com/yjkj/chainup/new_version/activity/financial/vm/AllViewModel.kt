ae12fea2f60e6a012d11dc1cc73ecbfbfb4ed887
奔跑的狗子 <6234>
2022-04-21 17:23:53 +0800

    理财UI整理下代码

--- app/src/main/java/com/yjkj/chainup/new_version/activity/financial/vm/AllViewModel.kt
+++ app/src/main/java/com/yjkj/chainup/new_version/activity/financial/vm/AllViewModel.kt
@@ -7,44 +7,15 @@ import androidx.lifecycle.MutableLiveData
 import com.yjkj.chainup.BR
 import com.yjkj.chainup.R
 import com.yjkj.chainup.base.BaseViewModel
-import com.yjkj.chainup.new_version.activity.documentary.AddMoneyDialog
-import com.yjkj.chainup.new_version.activity.documentary.ClosePositionDialog
-import com.yjkj.chainup.new_version.activity.documentary.ShareDialog
-import com.yjkj.chainup.new_version.activity.documentary.WinAndStopDialog
 import me.tatarka.bindingcollectionadapter2.ItemBinding
 
 class AllViewModel : BaseViewModel(){
 
     var activity = MutableLiveData<FragmentActivity>()
-
-    interface OnItemListener {
-        fun onClick()
-
-    }
-
-    var onItemListener: OnItemListener = object : OnItemListener {
-        override fun onClick() {
-        }
+    var page = MutableLiveData<Int>()
 
 
-    }
 
-    fun onShareClick() {
-        ShareDialog(). showDialog(activity.value?.supportFragmentManager,"")
-    }
-
-    fun onShareClick1() {
-        AddMoneyDialog(). showDialog(activity.value?.supportFragmentManager,"")
-    }
-
-    fun onShareClick2() {
-        WinAndStopDialog(). showDialog(activity.value?.supportFragmentManager,"")
-    }
-
-    fun onShareClick3() {
-        ClosePositionDialog(). showDialog(activity.value?.supportFragmentManager,"")
-
-    }
 
     val itemBinding =
         ItemBinding.of<String>(BR.item, R.layout.item_all)
