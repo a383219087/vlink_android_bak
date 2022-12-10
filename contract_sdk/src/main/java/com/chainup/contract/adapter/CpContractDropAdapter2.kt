b5d95015f0238a93e827a2397c198ae9304efe7a
liao'shu'dong <moxuanxiaoya@163.com>
2022-12-10 22:23:36 +0800

    首页合约item点击

--- contract_sdk/src/main/java/com/chainup/contract/adapter/CpContractDropAdapter2.kt
+++ contract_sdk/src/main/java/com/chainup/contract/adapter/CpContractDropAdapter2.kt
@@ -1,19 +1,21 @@
 package com.yjkj.chainup.new_contract.adapter
 
+import android.content.Intent
 import android.view.View
 import android.widget.LinearLayout
 import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.blankj.utilcode.util.ActivityUtils.startActivity
+import com.blankj.utilcode.util.ToastUtils
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.viewholder.BaseViewHolder
 import com.chainup.contract.R
+import com.chainup.contract.app.CpParamConstant
 import com.chainup.contract.eventbus.CpEventBusUtil
 import com.chainup.contract.eventbus.CpMessageEvent
-import com.chainup.contract.utils.CpBigDecimalUtils
-import com.chainup.contract.utils.CpClLogicContractSetting
-import com.chainup.contract.utils.CpColorUtil
-import com.chainup.contract.utils.CpNumberUtil
+import com.chainup.contract.ui.activity.CpMarketDetail4Activity
+import com.chainup.contract.utils.*
 import org.json.JSONObject
 import java.util.*
 
@@ -21,6 +23,13 @@ import java.util.*
  * Created by zj on 2018/3/7.
  */
 class CpContractDropAdapter2(data: ArrayList<JSONObject>) : BaseQuickAdapter<JSONObject, BaseViewHolder>(R.layout.cp_item_contract_drop2, data) {
+
+    var currentSymbol = "e_btcusdt"
+    var quote = ""
+    var base = ""
+    var mContractId = 1
+    var contractType = "0"
+    var symbolPricePrecision = 2
 
     class SpotViewHolder(itemView: View, type: Int) : RecyclerView.ViewHolder(itemView) {
         var rlContent: RelativeLayout
@@ -58,12 +67,33 @@ class CpContractDropAdapter2(data: ArrayList<JSONObject>) : BaseQuickAdapter<JSO
                 }
             }
             helper?.getView<LinearLayout>(R.id.rl_content)?.setOnClickListener {
+                mContractId = ticker.getInt("id")
+                base = ticker.getString("base")
+                quote = ticker.getString("quote")
+                contractType = ticker.getString("contractType")
+                currentSymbol = (ticker.getString("contractType") + "_" + ticker.getString("symbol")
+                .replace("-", "")).toLowerCase()
+                symbolPricePrecision = CpClLogicContractSetting.getContractSymbolPricePrecisionById(context,mContractId)
                 CpClLogicContractSetting.setContractCurrentSelectedId(context, ticker.optInt("id"))
                 var msgEvent =
                     CpMessageEvent(CpMessageEvent.sl_contract_left_coin_type)
                 msgEvent.msg_content = ticker
                 CpEventBusUtil.post(msgEvent)
+                startItem()
             }
+        }
+    }
+
+    fun startItem(){
+        if (!CpChainUtil.isFastClick()) {
+            val mIntent = Intent(context, CpMarketDetail4Activity::class.java)
+            mIntent.putExtra(CpParamConstant.symbol, currentSymbol)
+            mIntent.putExtra("contractId", mContractId)
+            mIntent.putExtra("baseSymbol",  base)
+            mIntent.putExtra("quoteSymbol",quote)
+            mIntent.putExtra("pricePrecision",symbolPricePrecision)
+            mIntent.putExtra(CpParamConstant.TYPE, CpParamConstant.BIBI_INDEX)
+            context.startActivity(mIntent)
         }
     }
 
